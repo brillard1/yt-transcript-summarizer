dfe8ad693453058332209248dd8889023cac084e
Soumil Sinha <sinha.5@iitj.ac.in>
2022-04-14 20:53:13 +0530

    commented code

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
+# Flask imports
 from flask import Flask, request, render_template
 
+# Summarizer
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
@@ -11,17 +13,21 @@ from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
+# YouTube Transcript API
 from youtube_transcript_api import YouTubeTranscriptApi
 
+# Flask local environment
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
+# Landing Page
 @app.route('/', methods=['GET','POST'])
 def send_form():
     return render_template('main.html', data="No video chosen!")
 
+# Summary Page
 @app.route('/summary', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
@@ -31,16 +37,27 @@ def data():
 
     return render_template('main.html', data = getSummary(form_data))
 
+# Function to generate summary of transcript
 def getSummary(form_data):
+
+    # Handling both types of URL inputs
+
     url1 = form_data['URL'].replace("https://www.youtube.com/watch?v=", "")
     url2 = form_data['URL'].replace("https://youtu.be/", "")
+
+    # Trying to get transcript using first type of URL
     try:
         transcript_list = YouTubeTranscriptApi.list_transcripts(url1)
     except:
+        # If the above fails, we use the second type of URL
         try:
             transcript_list = YouTubeTranscriptApi.list_transcripts(url2)
         except:
+            # If both types of URL fail, we return an error message
             return "Error! Video not found!"
+
+    # Extracting English transcript from the list of transcripts
+
     transcript = transcript_list.find_transcript(['en']).fetch()
     t = ""
     for i,T in enumerate(transcript):
@@ -48,6 +65,8 @@ def getSummary(form_data):
 
     LANGUAGE = "english"
     SENTENCES_COUNT = form_data['num_sentences']
+
+    # Generating summary
 
     parser = PlaintextParser.from_string(t, Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
